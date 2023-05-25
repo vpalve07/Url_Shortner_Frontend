f3a78841b6e3e7112c126626430793eb15e9f417
vpalve07 <vinayak.palve02@gmail.com>
2023-05-26 01:05:27 +0530

    first commit

---
+++ src/components/LinkResult.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+
+const LinkResult = ({ inputValue }) => {
+  const [shortenLink, setShortenLink] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError("")
+      const response = await axios.post("https://url-short-7i1b.onrender.com/url/shorten", {
+        longUrl: inputValue,
+      });
+      setShortenLink(response.data.data.shortUrl);
+    } catch (err) {
+      setError(err.response.data.msg);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (inputValue.length) {
+      fetchData();
+    }
+  }, [inputValue]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [copied]);
+
+  if (loading) {
+    return <p className="noData">Loading...</p>;
+  }
+  if (error) {
+    return <p className="noData">{error}</p>;
+  }
+
+  return (
+    <>
+      {shortenLink && (
+        <div className="result">
+          <p>{shortenLink}</p>
+          <CopyToClipboard text={shortenLink} onCopy={() => setCopied(true)}>
+            <button className={copied ? "copied" : ""}>Copy to Clipboard</button>
+          </CopyToClipboard>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LinkResult;
